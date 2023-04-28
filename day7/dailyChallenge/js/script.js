63d635371d1b619738cdc7c5dd19966f64d8c435
Anthony Ballard <antrball91@gmail.com>
2023-04-27 19:40:18 -0700

    daily

---
+++ day7/dailyChallenge/js/script.js
@@ -0,0 +1,7 @@
+console.log("welcome");
+var myFirstName = "Anthony";
+console.log(myFirstName);
+console.log(myFirstName.length)
+var luckyNumberOne = 1
+var luckyNumberTwo = 2
+console.log(`my First Lucky Number Is ${1} My second lucky Number Is ${2}`);
